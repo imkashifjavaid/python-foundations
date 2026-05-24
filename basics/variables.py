266f369fea034058f5672430953e23bf0dfbe843
kashif javaid <imkashifjavaid@gmail.com>
2026-05-24 17:36:10 +0300

    some more F strings

--- basics/variables.py
+++ basics/variables.py
@@ -1,5 +1,7 @@
 #Some Variables
 player_name = "Kashif Javaid"
+player_title = "Revealer"
+
 player_level = 1
 player_class = "beginner"
 player_age = 28
@@ -7,4 +9,4 @@ player_height = 5.7
 
 #print(player_name)
 
-# print(f"{player_name} has current level {player_level} and class {player_class}")
+print(f"{player_name} is a.k.a {player_title}")
