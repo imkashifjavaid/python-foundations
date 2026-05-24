e1611bda1e0e537f3231df5ea84d8fd29dfa33b4
kashif javaid <imkashifjavaid@gmail.com>
2026-05-24 11:54:40 +0300

    Add python variables, F strings, comments

---
+++ basics/variables.py
@@ -0,0 +1,10 @@
+#Some Variables
+player_name = "Kashif Javaid"
+player_level = 1
+player_class = "beginner"
+player_age = 28
+player_height = 5.7
+
+#print(player_name)
+
+# print(f"{player_name} has current level {player_level} and class {player_class}")
