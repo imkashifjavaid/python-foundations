b4cb8f47561a3759b596a152750d43a872843ff0
kashif javaid <imkashifjavaid@gmail.com>
2026-05-24 18:17:33 +0300

    adapted pythonic style removed braces

--- basics/If_conditions.py
+++ basics/If_conditions.py
@@ -3,7 +3,7 @@ num1 = 1
 num2 = 6
 
 #conditional statements
-if(num1 > num2):
+if num1 > num2:
     print(f" {num1} is greater than {num2}")
 else:
-    print(f"{num2} is greater")
+    print(f"{num2} is greater than {num1}")
