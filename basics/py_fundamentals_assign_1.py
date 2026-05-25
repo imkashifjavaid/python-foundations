bf445776f8802ec444b5b7da88236b51d37fa051
kashif javaid <imkashifjavaid@gmail.com>
2026-05-25 14:32:07 +0300

    python fundamentals assignment 1 (section 1 cleared)

---
+++ basics/py_fundamentals_assign_1.py
@@ -0,0 +1,16 @@
+#---------------------------
+#Section 1 - variable practices and f-strings
+#---------------------------
+#variables
+player_name = "Kashif"
+player_title = "Revealer"
+player_level = 1
+player_health = 100
+player_stamia = 100
+is_alive = True
+#f-string
+print(f" {player_name} a.k.a {player_title} is on level {player_level} with {player_health} health")
+
+#---------------------------
+#Section 2
+#---------------------------
