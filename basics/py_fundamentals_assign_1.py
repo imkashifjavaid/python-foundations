0b3bae5019e7fc7ba87fcc75e06a7f509f234734
kashif javaid <imkashifjavaid@gmail.com>
2026-05-25 14:41:57 +0300

    python fundamentals assignment 1 (section 2 cleared)

--- basics/py_fundamentals_assign_1.py
+++ basics/py_fundamentals_assign_1.py
@@ -1,16 +1,24 @@
+
 #---------------------------
-#Section 1 - variable practices and f-strings
+#Section 1 - Player Information (variable practices and f-strings)
 #---------------------------
-#variables
+#variables will be used in more than one sections for this assignment
 player_name = "Kashif"
 player_title = "Revealer"
 player_level = 1
 player_health = 100
-player_stamia = 100
+player_stamina = 100
 is_alive = True
 #f-string
-print(f" {player_name} a.k.a {player_title} is on level {player_level} with {player_health} health")
+print("Section 1 output:")
+print(f"{player_name} a.k.a {player_title} is on level {player_level} with {player_health} health")
 
 #---------------------------
-#Section 2
+#Section 2 - Health Check System
 #---------------------------
+print("Section 2 output:")
+
+if(player_health <= 0):
+    print(f"{player_name} died")
+else:
+    print(f"{player_name} is alive")
