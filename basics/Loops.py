a900de1b80ed7e175a4139d941f6c8117caeff43
kashif javaid <imkashifjavaid@gmail.com>
2026-06-01 13:51:29 +0300

    F string and loops

--- basics/Loops.py
+++ basics/Loops.py
@@ -6,5 +6,18 @@ for name in Names:
     print(name) '''
 
 #using range function
-"""for i in range(10):
+"""for i in range(2,10):
     print(i)"""
+
+"""for i in range(2,12,2): #start, stop before, jump of
+    print(i)"""
+
+"""for i in range(15):
+    print("HBD Kashif")
+"""
+
+Inventory_Items = ["Sword", "Shield", "Potions"]
+
+for item in Inventory_Items:
+    print("You have :", item) #Both have same outputs but in f string the FString joins first and then prints
+    print(f"You have: {item}")
