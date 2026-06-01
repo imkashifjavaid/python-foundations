acc993172a98e126e1178192d14fe2837d62204e
kashif javaid <imkashifjavaid@gmail.com>
2026-06-01 13:36:09 +0300

    Started loops

---
+++ basics/Loops.py
@@ -0,0 +1,10 @@
+# For Loops
+
+#Printing list using for loops
+''' Names = ["Sara", "Ali", "Usman", "Areeba","Hassan"]
+for name in Names:
+    print(name) '''
+
+#using range function
+"""for i in range(10):
+    print(i)"""
