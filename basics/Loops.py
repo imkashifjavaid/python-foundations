33ed8701f4fc1b44d88d62a516f3c516e010b8ab
kashif javaid <imkashifjavaid@gmail.com>
2026-06-01 14:35:52 +0300

    For loops basic questions done

--- basics/Loops.py
+++ basics/Loops.py
@@ -16,8 +16,32 @@ for name in Names:
     print("HBD Kashif")
 """
 
-Inventory_Items = ["Sword", "Shield", "Potions"]
+"""Inventory_Items = ["Sword", "Shield", "Potions"]
 
 for item in Inventory_Items:
     print("You have :", item) #Both have same outputs but in f string the FString joins first and then prints
-    print(f"You have: {item}")
+    print(f"You have: {item}")"""
+
+"""
+#Question 1 : # Output Weapon Equipped: Sword
+Weapon_List = ["Sword", "Axe", "Bow" ,"Dagger"]
+
+for weapon in Weapon_List:
+    print(f"Weapon Equipped {weapon}")
+"""
+
+
+"""
+#Question 2 : # Outputs Expected
+Party Member 1: Kashif
+Party Member 2: Ali
+Party Member 3: Sara
+Party Member 4: Ahmed
+"""
+
+Party_Member_Names = ["Ali", "Sara", "Ahmed"]
+count = 1
+
+for members in Party_Member_Names:
+    print(f"Party Member {count} : {members}")
+    count = count + 1
