6c77f0c4006d981d238262bd78e4424464a593a4
kashif javaid <imkashifjavaid@gmail.com>
2026-06-02 23:55:00 +0300

    solved practice problem 2 of continue statements

--- basics/break_continue.py
+++ basics/break_continue.py
@@ -16,12 +16,14 @@ if found == False:
     print("Employee never found") 
 '''
 
+
+
+#? Continue Problem Statement 1
+
 # * Continue Statement Work
 employees = [
     "Ali", "", "Sara", "", "Ahmed", "Fatima", ""]
-
 """
-#? Continue Problem Statement 1
 Expected Output
 Processing Employee: Ali
 Processing Employee: Sara
@@ -29,8 +31,34 @@ Processing Employee: Ahmed
 Processing Employee: Fatima
 """
 
-#? Continue Problem Statement 1 Solution
+'''#? Continue Problem Statement 1 Solution
 for employee  in employees:
     if employee  == "":
         continue
     print(f"Processing Employee: {employee }")
+'''
+
+
+'''
+#? Continue Problem Statement 2
+#? -1 means transaction failed
+#? Expected Output
+Processing transaction: 500
+Processing transaction: 1200
+Processing transaction: 300
+Processing transaction: 750 
+'''
+#? Continue Problem Statement 2 Solution
+transactions = [
+    500,
+    -1,
+    1200,
+    -1,
+    300,
+    750
+]
+
+for transaction  in transactions:
+    if transaction  == -1:
+        continue
+    print(f"Processing transaction: {transaction }")
