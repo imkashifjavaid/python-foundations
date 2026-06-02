54d802cd13eef1e0b9ef7137d265ddefb9cfd3ae
kashif javaid <imkashifjavaid@gmail.com>
2026-06-03 00:19:24 +0300

    Performing various list methods

---
+++ basics/lists.py
@@ -0,0 +1,13 @@
+
+#? List Data Set
+employees = [
+    "John Smith",
+    "Emily Johnson",
+    "Michael Brown",
+    "Sophia Davis",
+    "Daniel Wilson",
+    "Olivia Taylor"
+]
+
+#* 1_Printing total employees
+print(len(employees))
