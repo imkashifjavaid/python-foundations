ae20ed6b688c52f1d4b66921c26a97d45ac546cc
kashif javaid <imkashifjavaid@gmail.com>
2026-06-03 01:47:41 +0300

    debugged removed method in lists []

--- basics/lists.py
+++ basics/lists.py
@@ -9,12 +9,26 @@ employees = [
     "Olivia Taylor"
 ]
 
-#* Step 1 — Printing total employees
+"""#* Step 1 — Printing total employees
 print(len(employees))
-print(f"Total number of employees: {len(employees)}")
+print(f"Total number of employees: {len(employees)}")"""
 
-#* Step 2 — Replace First Employee
+"""#* Step 2 — Replace First Employee
 #*Requirement: John Smith -> Kashif
 print(f"Before Replacing Index 0: {employees}")
 employees[0] = "Kashif"
-print(f"After Replacing Index 0: {employees}")
+print(f"After Replacing Index 0: {employees}")"""
+
+"""#* Step 3 — Replace First Employee
+employees.append("Emma Watson")
+print(f"After appending: {employees}")"""
+
+#* Step 4 — Remove Employee 4 "Daniel Wilson"
+print(f"---Default List: {employees} ")
+employees.pop(4) #? removing the entry using index [POP]
+print(f"---Removed Daniel Wilson list: {employees} \n ")
+
+#print(employees, "\n")
+employees.remove("Michael Brown") #? removing the entry using value
+print(f"---Removed Michael Brown list: {employees}")
+
