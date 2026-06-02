3cf3a22c4bfe12c919943a970ce0817f38302a3b
kashif javaid <imkashifjavaid@gmail.com>
2026-06-03 01:59:04 +0300

    used some more list methods (index, insert)

--- basics/lists.py
+++ basics/lists.py
@@ -23,7 +23,7 @@ print(f"After Replacing Index 0: {employees}")"""
 employees.append("Emma Watson")
 print(f"After appending: {employees}")"""
 
-#* Step 4 — Remove Employee 4 "Daniel Wilson"
+"""#* Step 4 — Remove Employee 4 "Daniel Wilson"
 print(f"---Default List: {employees} ")
 employees.pop(4) #? removing the entry using index [POP]
 print(f"---Removed Daniel Wilson list: {employees} \n ")
@@ -31,4 +31,12 @@ print(f"---Removed Daniel Wilson list: {employees} \n ")
 #print(employees, "\n")
 employees.remove("Michael Brown") #? removing the entry using value
 print(f"---Removed Michael Brown list: {employees}")
+"""
 
+"""#* Step 5 — Finding Index of employee at
+print(f"List Display: {employees}")
+x = employees.index("Olivia Taylor")
+print(f"The Index value of {employees[5]} is {x}")"""
+
+employees.insert(0, "Kashif Javaid")
+print(employees)
