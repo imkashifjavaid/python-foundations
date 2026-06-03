afea85be837d6edc84b99c8db2c586b285ad406e
kashif javaid <imkashifjavaid@gmail.com>
2026-06-04 01:58:11 +0300

    Created simple employee database using list of dictionaries

---
+++ basics/list_of_dictionaries.py
@@ -0,0 +1,31 @@
+
+employee1 = {"employee_name": 'Emma Wilson',
+    "age" : 31,
+    "department" : 'Human Resources',
+    "salary": 92000,
+    "experience" : 7,
+    "location" : 'London',
+    "is_active" : True
+    }
+
+employee2 = {
+    "employee_name": 'Ryan Cooper',
+    "age" : 29,
+    "department" : 'Software Engineering',
+    "salary": 105000,
+    "experience" : 5,
+    "location" : 'Toronto',
+    "is_active" : True
+    }
+
+employee3 = {
+    "employee_name": 'Sophia Davis',
+    "age": 35,
+    "department": 'Finance',
+    "salary": 115000,
+    "experience": 10,
+    "location": 'New York',
+    "is_active": False
+}
+all_employees = [employee1, employee2, employee3]
+print(all_employees)
