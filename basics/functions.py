1d2c525b7d22211548e88fd9a076c75027b39c5f
Kashif Javaid <imkashifjavaid@gmail.com>
2026-06-05 09:25:33 +0300

    Learn Python functions with parameters and return values

--- basics/functions.py
+++ basics/functions.py
@@ -1,4 +1,28 @@
-def greet(): # function definition
+"""def greet(): # function definition
     print("Good Noon Kashif")
     
 greet() #function calls
+"""
+
+
+"""task 1
+def my_intro():
+    print("My name is Kashif")
+    print("Im learning python")
+    print("Im currently living in KSA")        
+my_intro()
+"""
+#task 2
+"""
+def sum_2(a, b):
+   # return num1 + num2
+    print(f"Sum: {a+b}")
+sum_2(2,2)
+"""
+#task 2 with return saved
+def sumx(a,b,c):
+    res = a + b + c 
+    print(f"Sum: {res}")
+    return 
+    
+sumx(2,2,2)
