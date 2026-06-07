8f050cf783d8faca48ed4ec14ccd90379dae61eb
kashif javaid <imkashifjavaid@gmail.com>
2026-06-07 22:59:07 +0300

    Practice Python functions, parameters and return statements

--- basics/functions.py
+++ basics/functions.py
@@ -20,18 +20,53 @@ def sum_2(a, b):
 sum_2(2,2)
 """
 #task 2 with return saved
-def sumx(a,b,c):
+'''def sumx(a,b,c):
     res = a + b + c 
     print(f"Sum: {res}")
     return 
     
 sumx(2,2,2)
+'''
 
 
 
-
-def show_employee(Name, Dep):
+'''def show_employee(Name, Dep):
     print(f"Employee Name: {Name}")
     print(f"Department: {Dep}")
 
 show_employee("Kashif Javaid", "Game Dev")
+'''
+
+'''#? Task 1 - Multiply Numbers
+
+def prod(a,b):
+    result = a * b
+    print(f"Product ={result}" )
+    return result
+
+prod(2,2)'''
+
+'''
+#? print Task 2 - Full Name
+
+def f_name(a,b):
+    result = a + " " + b
+    print(f"Full Name:  {result}")
+    return 
+
+f_name("Kashif", "Javaid")
+
+'''
+
+#Dry Running Code
+def multiply(a, b): #3,4
+    result = a * b #12
+    print(result)  #printed 12
+    return result #saved result 12
+
+x = multiply(3, 4) #12 used above
+
+print(x) # 12 will be printed 
+
+#final output: 12
+#            : 12
