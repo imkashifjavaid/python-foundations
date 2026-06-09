0d9ad5f263d808b196287a9fe8a08aef8c92931a
kashif javaid <imkashifjavaid@gmail.com>
2026-06-09 17:04:01 +0300

    Practice function with return values

--- basics/lists.py
+++ basics/lists.py
@@ -33,10 +33,55 @@ employees.remove("Michael Brown") #? removing the entry using value
 print(f"---Removed Michael Brown list: {employees}")
 """
 
+'''
 """#* Step 5 — Finding Index of employee at
 print(f"List Display: {employees}")
 x = employees.index("Olivia Taylor")
 print(f"The Index value of {employees[5]} is {x}")"""
 
 employees.insert(0, "Kashif Javaid")
-print(employees)
+print(employees)'''
+
+
+#? Task 1: Create Function employee_salary(name, salary)
+#? Expected Output:
+# Employee: Emma Wilson
+# Salary: 92000
+
+'''def employee_salary(name,salary):
+    print(f"Employee: {name} \nSalary: {salary}")
+
+#    return name
+employee_salary("Emma Wilson", 92000)
+'''
+
+
+
+"""
+#? Task 2: Create Function annual_salary(monthly_salary)
+
+Requirement:
+- Function should accept monthly_salary
+- Return yearly salary
+- Use return, not print
+
+Expected:
+
+result = annual_salary(5000)
+
+print(result)
+
+Output:
+60000
+"""
+
+monthly_salary = int(input("Enter your monthly salary: "))
+#a = 12
+def yearly_salary(monthly_salary):
+    result = monthly_salary * 12
+    return result
+
+annual_salary = yearly_salary(monthly_salary)
+
+print(f"Annual Salary: {annual_salary}")
+
