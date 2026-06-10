1d86bc90b20b8544e05f64be3fabef72ff429aa4
Kashif Javaid <imkashifjavaid@gmail.com>
2026-06-10 23:45:21 +0300

    Learn file creation, writing and reading in Python

--- basics/file_handling.py
+++ basics/file_handling.py
@@ -1,2 +1,21 @@
 with open("employees.txt", "w") as file:
     file.write("Kashif Javaid")
+
+
+
+with open("employees.txt", "w") as file:
+    file.write("Kashif Javaid")
+    
+    
+"""
+#? Task 2
+Read employees.txt
+Print its contents on screen
+Expected Output:
+Kashif Javaid
+"""
+
+with open("employees.txt", "r") as file:
+    data = file.read()
+
+print(data)
