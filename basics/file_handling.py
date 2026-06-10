d46e4261a47d2be9168832288ff95023c053b293
Kashif Javaid <imkashifjavaid@gmail.com>
2026-06-10 23:50:10 +0300

    Implement file append operations

--- basics/file_handling.py
+++ basics/file_handling.py
@@ -15,6 +15,31 @@ Expected Output:
 Kashif Javaid
 """
 
+"""
+with open("employees.txt", "r") as file:
+    data = file.read()
+
+print(data)
+"""
+
+"""
+#? Task 3
+
+Requirement:
+- Open employees.txt in append mode
+- Add a new employee named "Revealer"
+- Read the file again
+- Print complete file contents
+
+Expected Output:
+
+Kashif Javaid
+Revealer
+"""
+
+with open("employees.txt", "a") as file:
+    file.write("\nRevealer")
+
 with open("employees.txt", "r") as file:
     data = file.read()
 
